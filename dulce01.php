ad00e66743048e1c131e72e65ecc63d728f374a8
Dmoh22 <dulce34iwjgej@gmail.com>
2025-06-12 04:18:26 -0500

    Update dulce01.php

--- dulce01.php
+++ dulce01.php
@@ -1,133 +1,101 @@
-<!DOCTYPE html>
-   <html lang="en">
+<!DOCTYPE html> 
+<html lang="en">
 <head>
- 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="witdh=device-with, initial-scale=1.0">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<title>Dulce Maria Olmedo Hernandez</title>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    <link href="https://fonts.cdnfonts.com/css/boverky" rel="stylesheet">
-
-    <title>Dulce Maria Olmedo Hernandez</title>
-                
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <link href="https://fonts.cdnfonts.com/css/blood-crow" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/blood-crow" rel="stylesheet">
+    
+                    
+    
 </head>
+<body>
+<nav class="navbar navbar-light" style="background-color:rgb(0, 0, 0);">
+   <div class="container">
+    <a class="navbar-brand" href="index.html" style="color: white";>INICIO</a>
 
-        <body>
-            <nav class="navbar navbar-light bg-light">
-                <form class="form-inline">
-                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-                <div class="container">
-                <a class="navbar-brang" href="inde.html" style="color:white" ;>Inicio</a>
-                </div>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="nav navbar-nav">
+    <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="unidad1.html" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">UNIDAD 1</a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+    <a class="dropdown-item" href="/Demon/dulce01.php">Practica 1</a><br>
+    <a class="dropdown-item" href="/Demon/dulce02.php">Practica 2</a><br> 
+    <a class="dropdown-item" href="/Demon/dulce03.php">Practica 3</a><br>
+    </div>
+    </li>
+    </ul>
+    <ul class="nav navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="unidad1.html" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">UNIDAD 2</a>
+<div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+<a class="dropdown-item" href="/Demon/dulce04.php">Practica 4</a><br>
+<a class="dropdown-item" href="/Demon/dulce05a.php">Practica 5</a><br>
+<a class="dropdown-item" href="/Demon/dulce6.html">Practica 6</a><br>
+</li> </ul>
+<ul class="nav navbar-nav">
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="unidad1.html" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">UNIDAD 3</a>
+<div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+<a class="dropdown-item" href="/Demon/dulce07.html">Practica 7</a><br>
+<a class="dropdown-item" href="/Demon/dulce8.html">Practica 8</a><br> 
+<a class="dropdown-item" href="/Demon/final.html">Practica 9</a><br>
+    </div>
+</div>
+</div>
+</nav>
+   
+<div class="jumbotron">
+  <h1 class="display-4" style="text-align: center; color: rgb(0, 0, 0);
+  font-family: 'so this is it', blood-crow; text-shadow: 0 1 1 black;
+  ">Mostrar Datos</h1>
+  <?php
+  $username = "root";
+  $password = "";
+  $servername = "localhost";
+  $database = "Kimetsu_noyaiba";
+  $conexion = new mysqli($servername, $username, $password, $database);
+if($conexion->connect_error){
+  die( "La conexion fallo: " . $conexion->connect_error);
+}
+$sql = "SELECT * FROM demonslayer";
+$resultado = $conexion->query($sql);
+?>
+<div class="container">
+<h2>Datos de la tabla Demon Slayer</h2>
+<?php if ($resultado->num_rows >0): ?>
+  <table style bgcolor="white" border="1" align="center" bordercolor="black" cellpadding="4" cellspacing="4">
+      <tr>
+          <th>ID</th>
+          <th>Personaje</th>
+          <th>Carcteristicas </th>
+          <th>Rango</th>
+          <th>Respiración</th>
+          <th>Habilidades</th>
+      </tr>
+      <?php while ($fila = $resultado->fetch_assoc()): ?>
+          <tr>
+              <td><?php echo $fila['ID']; ?></td>
+              <td><?php echo $fila['Personaje']; ?></td>
+              <td><?php echo $fila['Caracteristicas']; ?></td>
+              <td><?php echo $fila['Rango']; ?></td>
+              <td><?php echo $fila['Respiración']; ?></td>
+              <td><?php echo $fila['Habilidades']; ?></td>
+          </tr>
+      <?php endwhile; ?>
+  </table>
+<?php else: ?>
+<p>No se encontraron los personajes</p>
+<?php endif; ?>
 
-              <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="nav navbar-nav">
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="unidad1.html" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                    style="color: white;">UNIDAD 1</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="/dulce01.html">Practica 1</a><br>
-                      <a class="dropdown-item" href="/dulce02.html">Practica 2</a><br>
-                      <a class="dropdown-item" href="/dulce03.html">Practica 3</a>
+</div>
 
-                    </div>
-                  </li>
-                </ul>
-                <ul class="nav navbar-nav">
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="unidad1.html" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                    style="color: white;">UNIDAD 1</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="/dulce04.html">Practica 4</a><br>
-                      <a class="dropdown-item" href="/dulce05.html">Practica 5</a><br>
-                      <a class="dropdown-item" href="/dulce06.html">Practica 6</a>
-
-                    </div>
-                  </li>
-                </ul>
-                <ul class="nav navbar-nav">
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="unidad1.html" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                    style="color: white;">UNIDAD 1</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="/dulce07.html">Practica 7</a><br>
-                      <a class="dropdown-item" href="/dulce08.html">Practica 8</a><br>
-                      <a class="dropdown-item" href="/dulce09.html">Practica 9</a><br>
-                      <a class="dropdown-item" href="/dulce10.html">Practica Final</a>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-
-
-
-
-
-              </nav>
-            <div class="jumbotron">
-                <h1 class="display-4" style="text-align: center;
-                color:coral;
-                font-family:'so this is it', sans-serif; text-shadow: 0 1 1 black;">
-                PAGINA MOSTRAR DATOS</h1>
-                <?php
-                $username="root";
-                $password="";
-                $servername = "localhost";
-                $database= "Animacion";
-                $conexion = new mysqli($username, $password, $servername, $database);
-                if($conexion->connect_error){
-                  die("La Conexion Fallo:" .$conexion->connect_error);
-
-                }
-                $sql = "SELECT * FROM  KDemon";
-                $resultado = "";
-                ?>
-                <?php
-                if($resultado->num_rows >0)?>{
-                  <table>
-                    <tr>
-                      <th>id</th>
-                      <th>Nombre Real</th>
-                      <th>Nombre de Superheroes</th>
-                      <th>Poderes</th>
-                      <th>Debilidades</th>
-                      <th>Bio</th>
-                </tr>
-                <?php while($fila = $resultado->fetch_assoc()); ?>
-                <tr>
-                  <td><?php echo $fila['id'];?></td>
-                  <td><?php echo $fila['nombrereal'];?></td>
-                  <td><?php echo $fila['id'];?></td>
-                  <td><?php echo $fila['id'];?></td>
-                  <td><?php echo $fila['id'];?></td>
-                  <td><?php echo $fila['id'];?></td>
-                  <td><?php echo $fila['id'];?></td>
-                  <td><?php echo $fila['id'];?></td>
-
-                </tr>
-                </table>
-
-                }
-
-                
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr class="my-4">
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p class="lead">
-                  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                </p>
-              </div>
-
-
-        </body>
-
-
-
-       
-       
+</div>
+</body>
+</html>
